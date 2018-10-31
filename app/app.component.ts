f4fda1ffa629ad11b6dcadd8af5a1e3242fb185c
eswarkumar28 <eswarkumar2893@gmail.com>
2018-10-31 23:53:26 +0530

    privary comments styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'First Opinion – The free way to talk to a doctor2';
 
-  /*images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);*/
 
-  /*images = [1, 2, 3].map(() => `/Users/rd/Desktop/Images=${Math.random()}`);*/
 
   images =[ '/src/images/6.png',
             '/src/images/19.png',
             '/src/images/6.png'];
 
+    
 
 }
